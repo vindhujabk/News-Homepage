ba1eea41328faba8cdedcc2f61e5010d6b438a49
Vindhuja BK <vindhuja.bk@gmail.com>
2024-03-22 19:46:00 +0530

    Designed home page

---
+++ index.js
@@ -0,0 +1,9 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidenavbar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidenavbar')
+    sidebar.style.display = 'none'
+}
